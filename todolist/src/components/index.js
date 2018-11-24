29edc1731ded76943c9268853cc6857b85ab3c36
soheejang15 <jsh167528@gmail.com>
2018-11-24 18:53:36 +0900

    ADD : components 폴더 내 컴포넌트 등록

---
+++ todolist/src/components/index.js
@@ -0,0 +1,11 @@
+import Title from './Title';
+import RegisterTodo from './RegisterTodo'
+import TodoList from './TodoList'
+import TodoItem from './TodoItem'
+
+export{
+  Title,
+  RegisterTodo,
+  TodoList,
+  TodoItem,
+}
